649a24828f5e3cb68d350be6dc1b7c0d24bd04bf
unknown <sashka.lysenko@gmail.com>
2018-10-09 22:50:47 +0300

    update testname

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -6,7 +6,7 @@ public class MainClassTest {
     MainClass MainClass = new MainClass();
 
     @Test
-    public void getLocalNumberTest(){
+    public void testGetLocalNumber(){
         Assert.assertEquals("MainClass.getLocalNumber() response != 14",
                 14, MainClass.getLocalNumber());
     }
